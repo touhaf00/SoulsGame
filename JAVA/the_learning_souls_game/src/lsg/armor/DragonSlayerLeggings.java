17d2cd950afe0818181ed75e53403b247568f10b
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-04 14:04:08 +0100

    Update DragonSlayerLeggings.java

--- JAVA/the_learning_souls_game/src/lsg/armor/DragonSlayerLeggings.java
+++ JAVA/the_learning_souls_game/src/lsg/armor/DragonSlayerLeggings.java
@@ -1,12 +1,17 @@
 package lsg.armor;
 
-public class DragonSlayerLeggings extends ArmorItem {
+
+public class DragonSlayerLeggings extends ArmorItem  {
 
 	private static String ARMOR_NAME = "Dragon Slayer Leggings" ;
 	private static float ARMOR_VALUE = 10.2f ; 
+	private static final int WEIGHT = 3;
 	
 	public DragonSlayerLeggings() {
 		super(ARMOR_NAME, ARMOR_VALUE) ;
 	}
-	
+	@Override
+	public int getWeight() {
+		return WEIGHT;
+	}
 }
