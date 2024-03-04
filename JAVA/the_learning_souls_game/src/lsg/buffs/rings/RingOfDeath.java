421c9f86cba08edd223520d8c7d7b8c4fedf0856
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-04 14:07:03 +0100

    Update RingOfDeath.java

--- JAVA/the_learning_souls_game/src/lsg/buffs/rings/RingOfDeath.java
+++ JAVA/the_learning_souls_game/src/lsg/buffs/rings/RingOfDeath.java
@@ -1,6 +1,5 @@
 package lsg.buffs.rings;
 
-import lsg.characters.Hero;
 
 public class RingOfDeath extends Ring{
 	
@@ -17,18 +16,6 @@ public class RingOfDeath extends Ring{
 			if(gauge <= LIMIT) return power ;
 			else return 0f ;
 		}else return 0f ;
-	}
-	
-	/**
-	 * Un test...
-	 * @param args non utilisé
-	 */
-	public static void main(String[] args) {
-		Hero hero = new Hero() ;
-		Ring r = new RingOfDeath() ;
-		hero.setRing(r, 1);
-		hero.getHitWith(60) ; // pour abaisser les PV du hero
-		System.out.println(r);
 	}
 	
 }
