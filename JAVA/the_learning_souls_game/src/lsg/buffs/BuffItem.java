08c2bdd4fe26760b50c133890f54ee4d4bb6310f
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-04 14:11:15 +0100

    Update BuffItem.java

--- JAVA/the_learning_souls_game/src/lsg/buffs/BuffItem.java
+++ JAVA/the_learning_souls_game/src/lsg/buffs/BuffItem.java
@@ -2,10 +2,12 @@ package lsg.buffs;
 
 import java.util.Locale;
 
-public abstract class BuffItem {
+import lsg.bags.Collectible;
+
+public abstract class BuffItem implements Collectible{
 	
 	private String name ; 
-	
+	private static final int WEIGHT= 1;
 	public BuffItem(String name) {
 		this.name = name ;
 	}
@@ -20,5 +22,8 @@ public abstract class BuffItem {
 	public String toString() {
 		return String.format(Locale.US, "[%s, %.2f]", getName(), computeBuffValue()) ;
 	}
-	
+	@Override
+    public int getWeight() {
+        return WEIGHT;
+    }
 }
