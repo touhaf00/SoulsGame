84007b5e586e7a4658266439d1790bf32ef3de8b
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-06 00:27:06 +0100

    Update SkillBar.java

--- JAVA/the_learning_souls_game/src/lsg/graphics/widgets/skills/SkillBar.java
+++ JAVA/the_learning_souls_game/src/lsg/graphics/widgets/skills/SkillBar.java
@@ -4,6 +4,7 @@ import java.util.LinkedHashMap;
 import javafx.geometry.Pos;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 
 public class SkillBar extends HBox {
 private static LinkedHashMap<KeyCode, String> DEFAULT_BINDING = new LinkedHashMap<>(); 
@@ -15,7 +16,12 @@ static{
         DEFAULT_BINDING.put(KeyCode.DIGIT5, "(");
     }
 
-    private SkillTrigger[] triggers ;
+    private SkillTrigger[] triggers ;  
+    private ConsumableTrigger consumableTrigger = new ConsumableTrigger(KeyCode.C, "c" , null, null);
+
+    public ConsumableTrigger getConsumableTrigger() {
+        return consumableTrigger;
+    }
 
     public SkillBar() {
         this.setSpacing(10);
@@ -32,14 +38,29 @@ static{
             this.getChildren().add(triggers[i]);
             i++;
         }
+
+        Rectangle spacer = new Rectangle(0, 0, 100, 50);
+        spacer.setOpacity(0);
+        this.getChildren().add(spacer);
+
+        this.getChildren().add(consumableTrigger);
     }
+    
     public SkillTrigger getTrigger(int slot){
         return triggers[slot];
     }
     public void process(KeyCode code){
         if(disabledProperty().get()) return;
+
+        if (code == consumableTrigger.getKeyCode())
+        {
+            consumableTrigger.trigger();
+            return;
+        }
+
         for(SkillTrigger trigger : triggers) {
             if(trigger.getKeyCode() == code) {
+
                 trigger.trigger();
                 break;
             }
