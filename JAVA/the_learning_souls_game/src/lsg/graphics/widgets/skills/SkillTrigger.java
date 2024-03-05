7fe0a90b3520395e9fb3f2ff69f86c4de92d6b9e
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-06 00:27:59 +0100

    Update SkillTrigger.java

--- JAVA/the_learning_souls_game/src/lsg/graphics/widgets/skills/SkillTrigger.java
+++ JAVA/the_learning_souls_game/src/lsg/graphics/widgets/skills/SkillTrigger.java
@@ -3,17 +3,20 @@ package lsg.graphics.widgets.skills;
 
 import javafx.animation.ScaleTransition;
 import javafx.scene.control.Label;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+
 public class SkillTrigger extends AnchorPane {
     private ImageView view;
     private Label text;
     private KeyCode keyCode;
     private SkillAction action; 
+    private ColorAdjust desaturate = new ColorAdjust(0, -1, 0.6, 0);
 
     public KeyCode getKeyCode() {
         return this.keyCode;
@@ -73,6 +76,13 @@ public class SkillTrigger extends AnchorPane {
     }
     private void addListeners() {
         setOnMouseClicked(event -> trigger());
+        disabledProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue) {
+                setEffect(desaturate);
+            }else {
+                setEffect(null);
+            }
+        });
     }
 
     private void animate() {
