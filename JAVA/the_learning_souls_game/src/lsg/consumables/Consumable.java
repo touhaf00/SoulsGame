87c5fcc69173b2a90f385940218160d784546164
touhaf00 <154981109+touhaf00@users.noreply.github.com>
2024-03-06 00:24:04 +0100

    Update Consumable.java

--- JAVA/the_learning_souls_game/src/lsg/consumables/Consumable.java
+++ JAVA/the_learning_souls_game/src/lsg/consumables/Consumable.java
@@ -1,16 +1,22 @@
 package lsg.consumables;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import lsg.bags.Collectible;
+import lsg.exceptions.ConsumeEmptyException;
 
 public class Consumable implements Collectible {
     private String name;
     private int capacity;
     private String stat;
+    private SimpleBooleanProperty isEmpty;
+
+
     private static final int WEIGHT= 1;
     public Consumable (String name,int capacity,String stat){
         this.name=name;
         this.capacity=capacity;
         this.stat=stat;
+        this.isEmpty = new SimpleBooleanProperty(capacity==0);
     }
     public String getName(){
     return name;
@@ -23,11 +29,13 @@ public class Consumable implements Collectible {
     }
     public void setCapacity(int capacity){
         this.capacity=capacity;
+        isEmpty.set(capacity==0);
     }
-    public int use(){
-        int totalCapacity = capacity;
-        capacity = 0;
-        return totalCapacity;
+    public int use() throws ConsumeEmptyException{
+        int capacity = getCapacity();
+        if (capacity == 0) { throw new ConsumeEmptyException(this); }
+        setCapacity(0);
+        return capacity;
     }
     @Override
     public String toString() {
@@ -37,6 +45,9 @@ public class Consumable implements Collectible {
     public int getWeight() {
         return WEIGHT;
         
+    }
+    public SimpleBooleanProperty isEmpty() {
+        return isEmpty;
     }
 }
 
